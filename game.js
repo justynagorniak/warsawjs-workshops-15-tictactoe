ec5b38594e0a63e61a6fef17140bb9fd22a8135f
Justyna <j.anna.gorniak@gmail.com>
2018-01-07 11:13:50 +0100

    function init
    function declaration, event handlers etc

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
-var divs = document.querySelectorAll(".board > div");
 
-function initGame(){
+document.addEventListener('DOMContentLoaded', function(){      //funkcja anonimowa
+
+ //wywolanie funkcji najpierw powinno byc! hoisting, jakby bylo na koncu tez by dzialalo 
+initGame();
  
-}
+    //deklaracja funkcji 
+    function initGame(){
+        var fields = document.querySelectorAll('.board > div'); //pobiera i zwraca !tablice! divow
+
+        fields.forEach(fields => fields.addEventListener('click',fieldClickHandler));
+  }
+
+  function fieldClickHandler() {
+      console.log("Hello", this);
+  }
+
+});
+
+
+
